b605f45a3cbf6afebc166a6f8d2664f270fd755a
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-05-01 14:34:01 -0500

    Add NodeFactory function src/node.js

---
+++ src/node.js
@@ -0,0 +1,15 @@
+const Node = function NodeFactory(value) {
+  const addNeighbor = function addNeighbor(neighbor) {
+    const neighbors = Array.from(this);
+
+    if (neighbors.includes(neighbor)) return;
+
+    this[this.length] = neighbor;
+    this.length++;
+    return this.length;
+  }
+
+  return { addNeighbor, value, length: 0 };
+}
+
+module.exports = Node;
