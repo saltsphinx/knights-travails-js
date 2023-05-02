e6c3c7f7ab2a6e2a874960e5511afac17afb09a9
tremar thomas <81628964+saltsphinx@users.noreply.github.com>
2023-05-01 22:01:17 -0500

    Complete board factory board.js

--- src/board.js
+++ src/board.js
@@ -1,65 +1,54 @@
 const Node = require('./node');
-
-const KnightMoves = [
-  [-1, 2],
-  [1, 2],
-  [-1, -2],
-  [1, -2]
-  [2, 1],
-  [2, -1],
-  [-2, 1],
-  [-2, -1],
-]
+const Coordinates = require('./Coordinates');
+const Moves = require('./Moves');
 
 const Board = function BoardFactory() {
-  const generateBoard = function generateBoard() {
-    const node = Node([0, 0]);
-    generateNeighbors(node);
+  const generateBoard = function generateBoard()
+  {
+    
+    const nodes = {};
 
-    return node;
-  };
+    Coordinates.forEach(function generateNodes(coordinate) {
+      const node = Node(coordinate);
 
-  let generateNeighbors = function generateNeighbors(node, index = 0, neighbors = [node], neighborCoordinates = [node.coordinate]) {
-    // if (index == neighbors.length) return;
-    if (index == neighbors.length)
-    {
-      console.log(neighbors.length);
-      return;
-    }
+      Moves.forEach(function addNeighbors(move) {
+        const neighborCoordinate = combineCoordinates(coordinate, move);
 
-    index++;
+        if (!isValidCoordinate(neighborCoordinate)) return;
+        const neighbor = nodes[neighborCoordinate.toString()];
 
-    KnightMoves.forEach(function generateNode(move) {
-      const newCoordinate = combineCoordinates(move, node.coordinate);
+        if (!neighbor) return;
+        node.addNeighbor(neighbor);
+        neighbor.addNeighbor(node);
+      });
 
-      if (!isValidCoordinate) return;
-      const coordinateIndex = neighborCoordinates.indexOf(newCoordinate);
-
-      let neighbor;
-      if (coordinateIndex >= 0)
-      {
-        neighbor = neighbors[coordinateIndex];
-      } else {
-        neighbor = Node(newCoordinate);
-        neighbors.push(neighbor);
-        neighborCoordinates.push(newCoordinate);
-      }
-
-      node.addNeighbor(neighbor);
-      neighbor.addNeighbor(node);
+      nodes[node.coordinate.toString()] = node;
     });
 
-    generateNeighbors(neighbors[index], index, neighbors, neighborCoordinates);
-  };
+    return nodes;
+  }
+
+  const compareCoordinates = function compareCoordinates(arr1) {
+    return (arr2) => {
+      if (arr1[0] == arr2[0] && arr1[1] == arr2[1]) return true;
+      return false;
+    }
+  }
 
   const combineCoordinates = function combineCoordinates(arr1, arr2) {
     return [arr1[0] + arr2[0], arr1[1] + arr2[1]];
   };
 
   const isValidCoordinate = function isValidCoordinate(coordinate) {
-    if (coordinate[0] < 0 || coordinate[0] > 7) return false
-    else if (coordinate[1] < 0 || coordinate[1] > 7) return false;
-    return true;
+    if (coordinate[0] >= 0 && coordinate[0] <= 7)
+    {
+      if (coordinate[1] >= 0 && coordinate[1] <= 7)
+      {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   return generateBoard();
